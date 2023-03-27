623902da3ec5516e4b918fe20bce61a28375eb59
TH.ThiagoAvels <thiagoavels1@gmail.com>
2023-03-26 23:27:45 -0300

    iniciando projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+// <img src="aa" alt="logo" />
+
+export default function Hearder() {
+    return (
+        <main>
+            <div>
+                Logo
+            </div>
+            <div>
+                Menu
+                <nav>
+
+                </nav>
+            </div>
+        </main>
+    )
+}
