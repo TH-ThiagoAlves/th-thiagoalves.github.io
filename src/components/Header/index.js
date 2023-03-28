7bce872cb9f89201525c13dbe7b30dc75cd6f938
TH.ThiagoAvels <thiagoavels1@gmail.com>
2023-03-27 23:53:30 -0300

    feat: logo definida

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import './style.css';
+import logo from '../../logo.svg';
+import teste from '../../assests/img/teste01.png';
 // <img src="aa" alt="logo" />
 
 export default function Hearder() {
     return (
         <main>
-            <div>
-                Logo
+            <div className="container-logo">
+                <img src={logo} alt="logo-fundo" />
+                <img src={teste} alt="log" />
             </div>
             <div>
                 Menu
