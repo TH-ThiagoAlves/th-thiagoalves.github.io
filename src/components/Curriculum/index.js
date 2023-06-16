e21761cb301a8e0069722e59846649ffac9d24d3
TH.ThiagoAvels <thiagoavels1@gmail.com>
2023-06-16 02:46:49 -0300

    feat: titulo da section completo e estilizado

--- src/components/Curriculum/index.js
+++ src/components/Curriculum/index.js
@@ -6,12 +6,16 @@ import './style.css';
 export default function Curriculum() {
   const particlesContainerRef = useRef(null);
 
+  // Função para criar uma partícula quando o mouse se move
   const createParticle = (event) => {
+    // Cria um elemento <div> para representar a partícula
     const particle = document.createElement('div');
     particle.className = 'particle';
     particle.style.left = (event.clientX - 5) + 'px';
     particle.style.top = (event.clientY - 5) + 'px';
+    // Adiciona a partícula ao contêiner de partículas
     particlesContainerRef.current.appendChild(particle);
+    // Remove a partícula após 1 segundo
     setTimeout(() => {
       particle.remove();
     }, 1000);
@@ -26,7 +30,7 @@ export default function Curriculum() {
       <button type="button">
         <a href={cv} download>Download CV</a>
       </button>
-      <div className="particles-container" ref={particlesContainerRef}></div>
+      <div className="particles-container" ref={particlesContainerRef} />
     </section>
   )
 }
